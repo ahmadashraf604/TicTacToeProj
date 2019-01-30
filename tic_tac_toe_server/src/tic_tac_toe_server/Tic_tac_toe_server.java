89c2aa5dbfd02a4c2f1c9441ba1600fb22d2f564
ahmadashraf604 <ahmadashraf604@gmail.com>
2019-01-30 19:35:01 +0200

    server handeling exception when one anther server

--- tic_tac_toe_server/src/tic_tac_toe_server/Tic_tac_toe_server.java
+++ tic_tac_toe_server/src/tic_tac_toe_server/Tic_tac_toe_server.java
@@ -11,14 +11,21 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import model.DataBaseConnection;
 import common.Player;
+import java.io.IOException;
+import java.net.Socket;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.WindowEvent;
 import model.ServerImplemention;
 import views.ServerView;
@@ -71,22 +78,63 @@ public class Tic_tac_toe_server extends Application {
     }
 
     private boolean openRegistry() {
-        try {
-            server = new ServerImplemention(this);
-            if (registry == null) {
-                registry = LocateRegistry.createRegistry(2015);
-            } else {
-                registry = LocateRegistry.getRegistry(2015);
+        if (!isPortInUse(2015)) {
+            try {
+                server = new ServerImplemention(this);
+                if (registry == null) {
+                    registry = LocateRegistry.createRegistry(2015);
+                } else {
+                    registry = LocateRegistry.getRegistry(2015);
+                }
+                registry.rebind(SERVICE_NAME, server);
+                System.out.println("server on");
+                serverAvilable = true;
+                return true;
+            } catch (RemoteException ex) {
+                System.out.println("server is not av");
+                return false;
             }
-            registry.rebind(SERVICE_NAME, server);
-            System.out.println("server on");
-            serverAvilable = true;
-            return true;
-        } catch (RemoteException ex) {
-            System.out.println("server is not av");
-            ex.printStackTrace();
+        }else{
+            makeAlert("Error", "the server is already on");
+            System.exit(0);
             return false;
         }
+    }
+
+    boolean isPortInUse(int portNumber) {
+        boolean result;
+        try {
+            Socket s = new Socket("127.0.0.1", portNumber);
+            s.close();
+            result = true;
+        } catch (IOException e) {
+            result = false;
+        }
+        return result;
+    }
+    
+      private boolean setAlertSetting(Alert alert, String title, String body) {
+        alert.setTitle(title);
+        Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/backgroundB.png")));
+        alert.setHeaderText(null);
+        alert.setContentText(body);
+        ImageView imageView = new ImageView(
+                new Image(getClass().getResourceAsStream("/images/background.png")));
+        imageView.setFitHeight(50.0);
+        imageView.setFitWidth(50.0);
+        alert.setGraphic(imageView);
+        DialogPane dialogPane = alert.getDialogPane();
+        dialogPane.getStylesheets().add(
+                getClass().getResource("myDialogs.css").toExternalForm());
+        dialogPane.getStyleClass().add("myDialog");
+        Optional<ButtonType> action = alert.showAndWait();
+        return action.get() == ButtonType.OK;
+    }
+
+    public boolean makeAlert(String title, String body) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        return setAlertSetting(alert, title, body);
     }
 
     public List<Player> getPlayers() {
